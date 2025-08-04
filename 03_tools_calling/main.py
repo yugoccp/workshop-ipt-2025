f69696f124085d0938c574a0c83198a7b4141f19
yugoccp <yugoccp@gmail.com>
2025-08-03 23:11:26 -0300

    feat: enhance prompt for 03_tool_calling example

--- 03_tools_calling/main.py
+++ 03_tools_calling/main.py
@@ -9,49 +9,52 @@ GRAPHDB_SCHEMA_PATH = GRAPHDB_PATH / "cypher/schema.cypher"
 GRAPHDB_DATABASE_PATH = GRAPHDB_PATH / "database/database.kuzu"
 OLLAMA_MODEL_NAME = "llama3.2"
 
+@tool
+def query_graph_tool(cypher_query: str) -> list:
+    """Retrieve GRAPH_SCHEMA database data given a single syntactically correct Cypher query.
+
+    Args:
+        cypher_query: a single syntactically correct Cypher query string to execute over GRAPH_SCHEMA database
+    """
+    try:
+        db = kuzu.Database(GRAPHDB_DATABASE_PATH)
+        conn = kuzu.Connection(db)
+        cypher_query_result = conn.execute(cypher_query)
+        return cypher_query_result.get_all()
+    except RuntimeError as e:
+        return f"""
+            Failed to run given query. Review the GRAPH_SCHEMA and try again.
+            Error details: {e}
+        """
+
 def main():
     """Main function to set up the graph database and process user queries."""    
 
     if not GRAPHDB_DATABASE_PATH.exists():
         raise SystemError("Run script `graphdb/init_graphdb.sh` script before running this example.")
-   
+
+    # Prepare assistant System Message
     graph_schema = GRAPHDB_SCHEMA_PATH.read_text()
     system_message = f"""
-        You are a helpful assistant.
-        Answer user questions based ONLY on the graph database data.
-        Reply in a concise, natural and informative manner.
-        DON'T include query details or database schema on final response.
+        You are an Graph Database expert with access to GRAPH_SCHEMA database.
+        Your task is to generate a syntactically correct Cypher query based on the provided GRAPH_SCHEMA to answer user question.
+        - STRICTLY use GRAPH_SCHEMA database data to answer user question.
+        - DON'T include query details or GRAPH_SCHEMA on final response.
+        - Reply in a concise and natural manner.
 
         <GRAPH_SCHEMA>
         {graph_schema}
         </GRAPH_SCHEMA>
     """
     
-    db = kuzu.Database(GRAPHDB_DATABASE_PATH)
-    conn = kuzu.Connection(db)
+    chat_model = ChatOllama(model=OLLAMA_MODEL_NAME)
+    chat_model = chat_model.bind_tools([query_graph_tool])
+    agent = create_react_agent(chat_model, [query_graph_tool])
+    agent = agent.with_config(recursion_limit=15)
 
     # Get a query from the user
     user_message = input("\nEnter your query: ")
 
-    @tool
-    def query_graph_tool(cypher_query: str) -> list:
-        """Execute a Cypher query on the GRAPH_SCHEMA database and return the results.
-
-        Args:
-            cypher_query: a valid Cypher query string to execute on GRAPH_SCHEMA database
-        """
-        try:
-            cypher_query_result = conn.execute(cypher_query)
-            return cypher_query_result.get_all()
-        except RuntimeError as e:
-            return f"""
-                Failed to run given query. Review the GRAPH_SCHEMA and try again.
-                Error details: {e}
-            """
-
-    chat_model = ChatOllama(model=OLLAMA_MODEL_NAME)
-    agent = create_react_agent(chat_model, [query_graph_tool])
-    
     print("\nGenerating response...")
 
     messages = [
