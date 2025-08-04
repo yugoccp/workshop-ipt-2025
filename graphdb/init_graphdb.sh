036c50fa8069fbfc5e3f4216691426029dbfcfd7
yugoccp <yugoccp@gmail.com>
2025-08-03 23:11:50 -0300

    change sample data to medical disease mapping data

--- graphdb/init_graphdb.sh
+++ graphdb/init_graphdb.sh
@@ -1,6 +1,18 @@
+echo "Building database Schema..."
 kuzu ./database/database.kuzu < ./cypher/schema.cypher
+
+echo "Loading CSV data to Graph..."
 kuzu ./database/database.kuzu < ./cypher/data.cypher
-docker run --rm -p 8000:8000 \
+
+CONTAINER_NAME="kuzudb_explorer"
+
+if docker ps --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo "Stopping existing kuzudb/explorer container..."
+    docker stop "${CONTAINER_NAME}"
+fi
+
+echo "Starting kuzudb/explorer container..."
+docker run --rm -d --name "${CONTAINER_NAME}" -p 8000:8000 \
     -v ./database:/database \
     -e KUZU_FILE="database.kuzu" \
     kuzudb/explorer:latest
